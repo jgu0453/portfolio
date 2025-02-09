5ce43bdf74a74d1ebeae2369371a6ab95d340c85
jgu0453 <119398520+jgu0453@users.noreply.github.com>
2025-02-08 20:57:15 -0800

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,11 +1,1 @@
-import { fetchJSON, renderProjects } from '../global.js';
 
-document.addEventListener('DOMContentLoaded', async () => {
-  const projectsContainer = document.querySelector('.projects');
-  const projects = await fetchJSON('../lib/projects.json');
-  if (projects) {
-    renderProjects(projects, projectsContainer);
-  } else {
-    projectsContainer.innerHTML = '<p>Failed to load projects.</p>';
-  }
-});
